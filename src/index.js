2a5e5f748835f7a4acaca88203fbc645e5525275
TheRealColeG <98725167+TheRealColeG@users.noreply.github.com>
2023-07-10 16:36:09 -0400

    Got the role editor working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, IntentsBitField, REST, Routes } = require('discord.js');
-const loginToken = require('./loginToken.json');
+const config = require('./config.json');
 
 //Remember to add the intents you need!!!!!!!!!!!!
 const client = new Client({
@@ -12,13 +12,6 @@ const client = new Client({
     ]
 });
 
-const commands = [
-    {
-        name: 'getEvents',
-        description: '(Un)enrolls you in receiving event notifs from the bot'
-    }
-]
-
 //When the bot is online, it'll log this message
 client.on('ready', (c) => {
     console.log(`Logged in as ${c.user.tag}!`);
@@ -28,18 +21,41 @@ client.on('ready', (c) => {
 When the bot sees a message in any of the servers that it is in (that it has access to), 
 it'll log the username of the author of the message (as of right now - might change later if needed) */
 client.on('messageCreate', (msg) => {
+    //So bot doesn't reply to itself/other bots - feedback loop
+    if (msg.author.bot) {
+        return;
+    }
+
+    //Logs the message in the console
     console.log(msg.author.username + ": " + msg.content);
+
+    //If the message starts with "ping", bot will reply with "pong"
+
 });
 
 //When the user types in a command, bot will take appropriate action
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-    const command = commands.find(command => command.name === interaction.commandName);
-    if (interaction.commandName === 'getEvents') {
-        await interaction.reply('Pong!');
-    }
+    if (interaction.commandName === 'getevents') {
+        //if user doesnt have the wildy event role, give it to them and reply
+        if (!interaction.member.roles.cache.some(role => role.name === 'Wildy Events')) {
+            try {
+                await interaction.member.roles.add('1128051399062720625');
+            } catch (error) {
+                console.error(`Failed to add role: Events - ${error}`);
+            }
+            await interaction.reply('User enrolled in event notifications.');
+        } else {
+            try {
+                await interaction.member.roles.remove('1128051399062720625');
+            } catch (error) {
+                console.error(`Failed to remove role: Events - ${error}`);
+            }
+            await interaction.reply('User unenrolled from event notifications.');
+        }
+    };
 });
 
 //Logs bot in using token from loginToken.json (which is gitignored)
-client.login(loginToken.TOKEN);
+client.login(config.TOKEN);
---
+++ src/register-commands.js
@@ -0,0 +1,27 @@
+const config = require('./config.json');
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+    {
+        name: 'getevents',
+        description: '(Un)enrolls you in receiving event notifs from the bot',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(config.TOKEN);
+
+(async () => {
+    try {
+        console.log('Registering application (/) commands.');
+
+        await rest.put (
+            Routes.applicationGuildCommands(config.BOT_ID, config.BOT_TEST_GUILD_ID),
+            { body: commands}
+        );
+
+        console.log('Successfully registered application (/) commands.');
+    }
+    catch (error) {
+        console.log(`There was an error: ${error}`);
+    }
+})();
